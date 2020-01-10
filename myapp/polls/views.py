d7b14caac7bf9c38f591e27fcc1a19746a05d9e0
raizengfilosofuxd <rickdavidsangalang@gmail.com>
2020-01-10 20:40:38 +0800

    showing data in my database

--- myapp/polls/views.py
+++ myapp/polls/views.py
@@ -3,7 +3,11 @@ from django.http import HttpResponse
 from .models import Event
 # Create your views here.
 def index (request):
+
+    e = Event.objects.all()
+    context = {
+        'title': 'This is my ',
+        'data': e
+    }
     
-    return render(request, 'posts/index.html',{
-        'title': 'HHHHHHHHHELLLO'
-    })
+    return render(request, 'posts/index.html',context)
