47b776534f0d7a272c5bacbb01cbee69f7507bb2
raizengfilosofuxd <rickdavidsangalang@gmail.com>
2020-01-10 18:41:13 +0800

    change models

--- myapp/polls/views.py
+++ myapp/polls/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Event
 # Create your views here.
 def index (request):
+    
     return render(request, 'posts/index.html',{
         'title': 'HHHHHHHHHELLLO'
     })
