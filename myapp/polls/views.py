7d595085611d33abcb3b3fcc7391cb1de525de7f
raizengfilosofuxd <rickdavidsangalang@gmail.com>
2020-01-10 13:08:47 +0800

    changes occur

--- myapp/polls/views.py
+++ myapp/polls/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index (request):
-    return HttpResponse('Hello')
+    return render(request, 'posts/index.html',{
+        'title': 'HHHHHHHHHELLLO'
+    })
